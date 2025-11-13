0ace495d19cd4d83d3857da5e834d46e0d3e824f
JovankaTangkilisan <s22210548@student.unklab.ac.id>
2025-11-13 16:40:55 +0800

    dasar project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-indigo-800 text-gray-200 text-center py-4 mt-10">
+      <p>© {new Date().getFullYear()} Jovanka Tangkilisan. All rights reserved.</p>
+    </footer>
+  );
+}
